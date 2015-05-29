501857f2aed7ab0b5d8f13f6cf54d13549912a41
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2015-05-29 15:58:33 -0400

    EC

--- HeadersManager.php
+++ HeadersManager.php
@@ -5,4 +5,8 @@ static function addH1($text){
 	echo '<h1>'.$text.'</h1>';
 
 } 
+static function addH2($text){
+	echo "<h2>".$text."</h2>";
+	
+}
 }
---
+++ Manager.php
@@ -0,0 +1,8 @@
+<?php 
+  namespace ArcherSys\APIGen;
+  abstract class AbstractManager {
+
+
+
+}
+?>
